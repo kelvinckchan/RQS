a7056951388ac5e21fde0d51233c52ec24f2d8d3
sokwokyuk <sokwokyuk@158.182.203.145>
2017-11-24 23:15:28 +0800

    ?

--- AppKickstarter/src/AppKickstarter/Gui/TestStaffPanel.java
+++ AppKickstarter/src/AppKickstarter/Gui/TestStaffPanel.java
@@ -112,13 +112,13 @@ public class TestStaffPanel extends JPanel {
 		TablePanel.setBackground(Color.WHITE);
 		TablePanel.setBounds(8, 11, 712, 351);
 		StaffPanel.getContentPane().add(TablePanel);
-		
+
 		TablePanel.setLayout(null);
 
-//		JScrollPane scrollPane_1 = new JScrollPane();
-//		scrollPane_1.setBounds(0, 6, 712, 351);
+		// JScrollPane scrollPane_1 = new JScrollPane();
+		// scrollPane_1.setBounds(0, 6, 712, 351);
 		t1 = new JList();
-		t1.setBounds(0, 0, 712 / 5, 351);
+		t1.setBounds(0, 6, 712, 351);
 		t2 = new JList();
 		t1.setBounds(142, 0, 712 / 5, 351);
 		t3 = new JList();
@@ -127,18 +127,18 @@ public class TestStaffPanel extends JPanel {
 		t1.setBounds(560, 0, 712 / 5, 351);
 		t5 = new JList();
 		t1.setBounds(1200, 0, 712 / 5, 351);
-		InsertDataToFloorPlan();
-//		scrollPane_1.add(t1);
-//		scrollPane_1.add(t2);
-//		scrollPane_1.add(t3);
-//		scrollPane_1.add(t4);
-//		scrollPane_1.add(t5);
 
-//		TablePanel.add(scrollPane_1);
+		// scrollPane_1.add(t1);
+		// scrollPane_1.add(t2);
+		// scrollPane_1.add(t3);
+		// scrollPane_1.add(t4);
+		// scrollPane_1.add(t5);
+
+		// TablePanel.add(scrollPane_1);
 
 		TablePanel.add(t1);
-		
-		
+		InsertDataToFloorPlan();
+
 		JPanel TicketPanel = new JPanel();
 		TicketPanel.setBackground(new Color(255, 255, 255));
 		TicketPanel.setBounds(732, 11, 251, 351);
@@ -159,28 +159,10 @@ public class TestStaffPanel extends JPanel {
 			}
 		}
 
-		MouseListener mouseListener = new MouseAdapter() {
-			public void mousePressed(MouseEvent mouseEvent) {
-				int modifiers = mouseEvent.getModifiers();
-				if ((modifiers & InputEvent.BUTTON3_MASK) == InputEvent.BUTTON3_MASK) {
-					System.out.println("Right button pressed.");
-
-				}
-			}
-
-			public void mouseReleased(MouseEvent mouseEvent) {
-				if (SwingUtilities.isRightMouseButton(mouseEvent)) {
-					System.out.println("Right button released.");
-
-				}
-			}
-		};
-
 	}
 
 	public void InsertDataToFloorPlan() {
-System.out.println("Table"+TableList.size());
-//		String[] TableHeading = new String[] { "Table 1-2", "Table 3-4", "Table 5-6", "Table 7-8", "Table 9-10" };
+		System.out.println("Table" + TableList.size());
 		List<Integer> tbWSize2 = TableList.stream().filter(t -> t.getTableSize() == 2).map(table -> table.getTableNo())
 				.collect(Collectors.toList());
 		List<Integer> tbWSize4 = TableList.stream().filter(t -> t.getTableSize() == 4).map(table -> table.getTableNo())
@@ -189,20 +171,23 @@ System.out.println("Table"+TableList.size());
 				.collect(Collectors.toList());
 		List<Integer> tbWSize8 = TableList.stream().filter(t -> t.getTableSize() == 8).map(table -> table.getTableNo())
 				.collect(Collectors.toList());
-		List<Integer> tbWSize10 = TableList.stream().filter(t -> t.getTableSize() == 10).map(table -> table.getTableNo())
-				.collect(Collectors.toList());
+		List<Integer> tbWSize10 = TableList.stream().filter(t -> t.getTableSize() == 10)
+				.map(table -> table.getTableNo()).collect(Collectors.toList());
 
 		DefaultListModel listModel1 = new DefaultListModel();
 		for (int i = 0; i < tbWSize2.size(); i++) {
 			listModel1.addElement(tbWSize2.get(i));
 		}
-		
+
 		t1.setModel(listModel1);
+		
+		
+		
 		DefaultListModel listModel2 = new DefaultListModel();
 		for (int i = 0; i < tbWSize4.size(); i++) {
 			listModel1.addElement(tbWSize4.get(i));
 		}
-		
+
 		t2.setModel(listModel2);
 		DefaultListModel listModel3 = new DefaultListModel();
 		for (int i = 0; i < tbWSize6.size(); i++) {
@@ -220,37 +205,5 @@ System.out.println("Table"+TableList.size());
 		}
 		t5.setModel(listModel1);
 
-		// List<Object> Col1 = new ArrayList<Object>();
-		// for (int t : tbWSize2) {
-		// System.out.println(t);
-		// }
-		//
-		// for (Table t : TableList) {
-		// }
-
-		// Object[][] AllTable = new Object[][] { tbWSize2, tbWSize4, tbWSize6,
-		// tbWSize8, tbWSize10 };
-
-		for (int t : tbWSize2) {
-			System.out.println(t);
-		}
-		System.out.println("------------");
-		for (int t : tbWSize4) {
-			System.out.println(t);
-		}
-		System.out.println("------------");
-		for (int t : tbWSize6) {
-			System.out.println(t);
-		}
-		System.out.println("------------");
-		for (int t : tbWSize8) {
-			System.out.println(t);
-		}
-		System.out.println("------------");
-		for (int t : tbWSize10) {
-			System.out.println(t);
-		}
-		// FloorPlan.setModel(new DefaultTableModel(list.toArray(new Object[][] {}), new
-		// String[] { "TableNo" }));
 	}
 }
